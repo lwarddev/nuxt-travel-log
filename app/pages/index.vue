35847bf05c008aa83cfa1d24bb13634c9284d5e0
LWardDev <lwarddev@gmail.com>
2025-08-09 22:40:08 +0100

    Added landing page

---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero bg-base-200 container mx-auto mt-4 p-24">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your travels with our cool travel log app.
+        </p>
+        <button class="btn btn-accent">
+          Sign In With Github
+          <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
