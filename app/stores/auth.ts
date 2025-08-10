d44855122ad581f7922ac083b601fc17554131d1
Ward Developments <lwarddev@gmail.com>
2025-08-10 21:03:02 +0100

    updated use session for SSR (#58)

--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -1,9 +1,15 @@
 import { authClient } from "~/lib/auth-client";
 
 export const useAuthStore = defineStore("useAuthStore", () => {
-  const session = authClient.useSession();
-  const user = computed(() => session.value.data?.user);
-  const loading = computed(() => session.value.isPending || session.value.isRefetching);
+  const session = ref<Awaited<ReturnType<typeof authClient.useSession>> | null>(null);
+
+  async function init() {
+    const data = await authClient.useSession(useFetch);
+    session.value = data;
+  }
+
+  const user = computed(() => session.value?.data?.user);
+  const loading = computed(() => session.value?.isPending);
 
   async function signIn() {
     await authClient.signIn.social({
@@ -19,6 +25,7 @@ export const useAuthStore = defineStore("useAuthStore", () => {
   }
   return {
     loading,
+    init,
     signIn,
     signOut,
     user,
